65ebfa4342d38d83c8344aa0aa23563aa37629e3
Quinton Fults <quinton.fults.work@gmail.com>
2020-12-17 10:41:31 -0800

    code

---
+++ routes/itemRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+
+router.get('/items', (req, res) => {
+
+})
+
+router.post('/items', (req, res) => {
+
+})
+
+router.put('/items/:id', (req, res) => {
+
+})
+
+router.delete('/items/:id', (req, res) => {
+
+})
+
+module.exports = router
